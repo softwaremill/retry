9dceb64b54805bf1c3fb54f28673d03fe27f7f76
softprops <d.tangren@gmail.com>
2013-05-28 00:02:22 -0400

    import over qualify

--- core/src/main/scala/sleep.scala
+++ core/src/main/scala/sleep.scala
@@ -2,13 +2,14 @@ package retry
 
 import scala.concurrent.{ ExecutionContext, Future, Promise }
 import scala.concurrent.duration.Duration
+import scala.util.Try
 
 object SleepFuture {
   def apply[T](d: Duration)(todo: => T)
               (implicit timer: Timer,
                executor: ExecutionContext): Future[T] = {
     val promise = Promise[T]()
-    timer(d.length, d.unit, promise.complete(util.Try(todo)))
+    timer(d.length, d.unit, promise.complete(Try(todo)))
     promise.future
   }
 }
