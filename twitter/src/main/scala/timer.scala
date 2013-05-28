0fe129822308ad9d9c647309ac3ec67b07a62786
softprops <d.tangren@gmail.com>
2013-05-28 00:02:02 -0400

    fixup twttr interface

--- twitter/src/main/scala/timer.scala
+++ twitter/src/main/scala/timer.scala
@@ -1,10 +1,13 @@
 package retry.twitter
 
 import java.util.concurrent.TimeUnit
-import com.twitter.util.{ Duration, Timer }
+import com.twitter.util.{ Duration, Timer, TimerTask }
+
+case class TwitterTimeout(underlying: TimerTask) extends retry.Timeout {
+  def cancel() = underlying.cancel()
+}
 
 case class TwitterTimer(underlying: Timer) extends retry.Timer {
-  def apply[T](length: Long, unit: TimeUnit, todo: => T) {
-    underlying.doLater(Duration.fromTimeUnit(length,unit))(todo)
-  }
+  def apply[T](length: Long, unit: TimeUnit, todo: => T): retry.Timeout =
+    TwitterTimeout(underlying.schedule(Duration.fromTimeUnit(length, unit))(todo))
 }
