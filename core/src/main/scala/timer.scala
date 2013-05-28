67cec81c5c3b65c680e682db0ab2a042533e50b8
softprops <d.tangren@gmail.com>
2013-05-28 00:03:56 -0400

    fix up and document interfaces

--- core/src/main/scala/timer.scala
+++ core/src/main/scala/timer.scala
@@ -2,6 +2,12 @@ package retry
 
 import java.util.concurrent.TimeUnit
 
+/** Represents a timeout emitted by the invocation of a timer operation */
+trait Timeout {
+  def cancel(): Unit
+}
+
+/** interface for executing a block of code at a given time */
 trait Timer {
-  def apply[T](length: Long, unit: TimeUnit, todo: => T): Unit
+  def apply[T](length: Long, unit: TimeUnit, todo: => T): Timeout
 }
--- netty/src/main/scala/timer.scala
+++ netty/src/main/scala/timer.scala
@@ -3,12 +3,15 @@ package retry.netty
 import java.util.concurrent.TimeUnit
 import org.jboss.netty.util.{ Timeout, Timer, TimerTask }
 
+case class NettyTimeout(underlying: Timeout) extends retry.Timeout {
+  def cancel() = underlying.cancel()
+}
+
 case class NettyTimer(underlying: Timer) extends retry.Timer {
-  def apply[T](length: Long, unit: TimeUnit, todo: => T) {
-    underlying.newTimeout(new TimerTask {
+  def apply[T](length: Long, unit: TimeUnit, todo: => T): retry.Timeout =
+    NettyTimeout(underlying.newTimeout(new TimerTask {
       def run(timeout: Timeout) {
         todo
       }
-    }, length, unit)
-  }
+    }, length, unit))
 }
