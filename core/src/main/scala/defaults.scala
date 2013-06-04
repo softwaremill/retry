8b1d53f092d93219ba250c4dba0391e2e97fd576
softprops <d.tangren@gmail.com>
2013-06-03 22:56:02 -0400

    restrict type

--- core/src/main/scala/defaults.scala
+++ core/src/main/scala/defaults.scala
@@ -1,9 +1,10 @@
 package retry
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ ExecutionContext,  ExecutionContextExecutor }
 
 object Defaults {
-  implicit def executor = ExecutionContext.Implicits.global
+  implicit def executor: ExecutionContextExecutor =
+    ExecutionContext.Implicits.global
   implicit val timer: Timer = jdk.Default.timer
 }
 
