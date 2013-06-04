1729e3f524077e87f03d2f3f22e2d186bbc68a24
softprops <d.tangren@gmail.com>
2013-06-03 22:56:35 -0400

    retry tests

--- core/src/test/scala/RetrySpec.scala
+++ core/src/test/scala/RetrySpec.scala
@@ -1,39 +1,75 @@
 package retry
 
 import org.scalatest.FunSpec 
-import scala.concurrent._
+import scala.annotation.tailrec
+import scala.concurrent.{ Future, Await }
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.Try
 
 class RetrySpec extends FunSpec {
-  case class FailsUntil(times: Int) {
-    @volatile
-    private var tried: Int = 0
-    def apply(): Future[Option[Boolean]] = {
-      val promise = Promise[Option[Boolean]]()      
-      val truth = if (times == tried) Some(true)
-      else {
-        tried += 1
-        None
-      }
-      promise.complete(Try(truth))
-      promise.future
-    }
+
+  def forwardCountingFutureStream(value: Int = 0): Stream[Future[Int]] =
+    Future(value) #:: forwardCountingFutureStream(value + 1)
+
+  def backwardCountingFutureStream(value: Int): Stream[Future[Int]] =
+    if (value < 0) Stream.empty
+    else Future(value) #:: backwardCountingFutureStream(value - 1)
+
+  def time[T](f: => T): Duration = {
+    val before = System.currentTimeMillis
+    f
+    Duration(System.currentTimeMillis - before, MILLISECONDS)
   }
 
   describe("retry.Directly") {
     it ("should retry a future for a specified number of times") {
-      val tries = FailsUntil(2)
-      assert(Await.result(retry.Directly(2)({ () => tries.apply() }), 10.millis) === Some(true))
+      implicit val success = new Success[Int](_ == 3)
+      val tries = forwardCountingFutureStream().iterator
+      val result = Await.result(retry.Directly(3)(tries.next),
+                                1.millis)
+      assert(success.predicate(result) === true)
     }
 
     it ("should fail when expected") {
-      val tries = FailsUntil(2)
-      assert(Await.result(retry.Directly(1)({ () => tries.apply() }), 10.millis) === None)
+      val success = implicitly[Success[Option[Int]]]
+      val tries = Future(None: Option[Int])
+      val result = Await.result(retry.Directly(2)({ () => tries }),
+                                1.millis)
+      assert(success.predicate(result) === false)
     }
   }
 
-  describe("retry.Pause") { }
-  describe("retry.Backoff") { }
+  describe("retry.Pause") {
+    it ("should pause in between retries") {
+      import retry.Defaults.timer
+      implicit val success = new Success[Int](_ == 3)
+      val tries = forwardCountingFutureStream().iterator
+      val took = time {
+        val result = Await.result(retry.Pause(3, 30.millis)(tries.next),
+                                  90.seconds + 20.millis)
+        assert(success.predicate(result) == true)
+      }
+      assert(took >= 90.millis === true,
+             "took less time than expected: %s" format took)
+      assert(took <= 110.millis === true,
+             "took more time than expected: %s" format took)
+    }
+  }
+
+  describe("retry.Backoff") {
+    it ("should pause with multiplier between retries") {
+      import retry.Defaults.timer
+      implicit val success = new Success[Int](_ == 2)
+      val tries = forwardCountingFutureStream().iterator
+      val took = time {
+        val result = Await.result(retry.Backoff(2, 30.millis)(tries.next),
+                                  90.millis + 20.millis)
+        assert(success.predicate(result) === true, "predicate failed")
+      }
+      assert(took >= 90.millis === true,
+             "took less time than expected: %s" format took)
+      assert(took <= 110.millis === true,
+             "took more time than expected: %s" format took)
+    }
+  }
 }
